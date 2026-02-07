c6ffc02d9c260fd9754e0a5ec475a3c4a54f04ad
Sanjeev Midda <s.midda1@gmail.com>
2026-02-07 20:48:59 +0000

    adding in UI for user data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import "./index.css";
 function App() {
   // purpose: Build a React app that displays a list of users and allows filtering the list using multiple controlled inputs.
 
+  // Requirements
+
+  //   3. Combined filtering logic
+  // Users must be filtered using both:
+  // - search text
+  // - selected role
+
+  // Filtering must be:
+  // - case-insensitive
+  // - resilient to leading/trailing spaces
+
+  // lf:
+  // - search is empty → do not filter by name
+  // - role is "All" → do not filter by
+
   // add user data
   const users = [
     { id: 1, name: "Alice Johnson", role: "Admin" },
@@ -16,7 +31,35 @@ function App() {
     { id: 5, name: "Jack Cork", role: "User" },
   ];
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* Display user data */}
+
+      <div className="mainUserDataContainer">
+        <div className="headings">
+          <h2>NAME</h2>
+          <h2>ROLE</h2>
+        </div>
+
+        <div className="userData">
+          {users.map((user) => {
+            return (
+              <div className="userinfoContainer">
+                <p>{user.name}</p>
+                <p>{user.role}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* Controlled inputs */}
+
+      {/* Search input */}
+
+      {/* Role filter */}
+    </div>
+  );
 }
 
 export default App;
